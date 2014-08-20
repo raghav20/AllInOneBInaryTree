848d8b8fc8a768e8d74db4104335db1d95b0b17a
Prashant <prash.raaghav@gmail.com>
2014-08-20 18:52:24 -0400

    Update BinaryTree.java

--- BinaryTree.java
+++ BinaryTree.java
@@ -43,11 +43,14 @@ public class BTree{
   BTreeNode root;
   static int min=0;
   Hashtable<Integer,ArrayList<BTreeNode>>ht;
+  public List<Integer> listI;
+  public List<Integer> listP;
   //Constructor
   BTree(){
     root  = null;
     ht = new Hashtable<Integer,ArrayList<BTreeNode>>();
-    
+    listI = new ArrayList<Integer>(); 
+    listP = new ArrayList<Integer>(); 
   }
 
   //inserting values into Binary Tree
@@ -61,18 +64,11 @@ public class BTree{
     }
   }
 
-  //preOrder traversal
-  public void preOrder(BTreeNode node){
-    if(node ==null)return;
-    System.out.println("preOrder "+ node.value);
-    preOrder(node.lChild);
-    preOrder(node.rChild);
-  }
-
   //inOrder traversal
   public void inOrder(BTreeNode node){
     if(node ==null)return;
     inOrder(node.lChild);
+    listI.add(node.value);
     System.out.println("InOrder "+ node.value);
     inOrder(node.rChild);
   }
@@ -348,8 +344,6 @@ public class BTree{
     int big = l+r+node.value;
     int res = Math.max(Math.max(l,r),big);
     if(res>sum)sum = res;
-    System.out.println("\nvalue "+res+ " left "+l+" right "+r+" big "+big);
-    System.out.println("\nvalue "+res+ " left "+l+" right "+r);
     min =res;
     return Math.max(l,r)+node.value;
   }
@@ -377,6 +371,7 @@ public class BTree{
     System.out.println(inOrderSuccessor(root,this.root.lChild.lChild).value);
     System.out.println("............................................................................");
     int a[] ={1,2,4,5,6,8,9};
+    int c[] ={2,4,5,6,8};
     System.out.println("............................................................................");
     inOrder(arraytoTree(a,0,6));
     //ArrayList<LinkedList<BTreeNode>> al = getList(root);
@@ -390,7 +385,16 @@ public class BTree{
     }
     System.out.println("............................................................................");
     spiralOrder(this.root);
+    System.out.println("............................................................................");
+    System.out.println(isCousin(root.lChild.lChild,root.rChild.lChild));
     
+    //BTreeNode t1 = arraytoTree(a,0,6);
+    //BTreeNode t2 = arraytoTree(c,0,4);
+    //System.out.println(isSubTree(t1,t2));
+    //System.out.println(setSumChild(this.root));
+    inOrder(root);
+    System.out.println("............................................................................");
+    System.out.println("BST "+isBST(this.root));
   }
   //print tree in vertical order
   public void inVertical(BTreeNode node,int hd){
@@ -439,7 +443,6 @@ public class BTree{
         if(tmp.value<anc.value){
           succ = anc;
           anc =anc.lChild;
-          System.out.println("ha ha "+succ.value);
         }
         else{
           anc = anc.rChild;
@@ -518,5 +521,96 @@ public class BTree{
 			}	
 		}
   }
-
+  
+  //getLevel
+  public int getLevel(BTreeNode node){
+    Hashtable<Integer,LinkedList<BTreeNode>> ht = getList(this.root);
+    Set<Integer> keys = ht.keySet();
+    for(Integer s: keys){
+      LinkedList<BTreeNode>l = ht.get(s);
+      for(BTreeNode b:l){
+        if(b == node) return s;
+      }
+    }
+    return -1;
+  }
+  
+  // 2 nodes are cousins 
+  public boolean isCousin(BTreeNode p,BTreeNode q){
+    if(p==null || q==null)return false;
+    if((getParent(p) != getParent(q)) && getLevel(p) == getLevel(q))return true;
+    else return false;
+  }
+  
+  //isSubTree
+  public boolean isSubTree(BTreeNode t1,BTreeNode t2){
+    List<Integer>l1 = inOrderTraversal(t1);
+    List<Integer>l2 = preOrderTraversal(t1);
+    List<Integer>l3 = inOrderTraversal(t2);
+    List<Integer>l4 = preOrderTraversal(t2);
+    if(Arrays.asList(l1).containsAll(Arrays.asList(l3))&&(Arrays.asList(l2).containsAll(Arrays.asList(l4))))return true;
+    else return false;
+  }
+  
+  //preOrder
+  public List<Integer> preOrderTraversal(BTreeNode node) {
+    preOrder(node);
+    return listP;
+  }
+  public void preOrder(BTreeNode node){
+    if(node==null)return;
+    listP.add(node.value);
+    if(node.lChild!=null)preOrder(node.lChild);
+    if(node.rChild!=null)preOrder(node.rChild);
+  }
+  
+  //postOrder
+  public List<Integer> inOrderTraversal(BTreeNode node){
+    inOrder(node);
+    return listI;
+  }
+  
+  //sum of children
+  public int setSumChild(BTreeNode node){
+    if(node.lChild==null && node.rChild==null)return node.value;
+    int prev = node.value;
+    node.value = setSumChild(node.lChild)+ setSumChild(node.rChild);
+    return prev;
+  }
+  //get maximum
+  public int getMaximum(BTreeNode node,int max){
+    if(node==null)return -1;
+    if(node.value>max)max = node.value;
+    int l=0;
+    int r=0;
+    if(node.lChild!=null)l=getMaximum(node.lChild,max);
+    if(node.rChild!=null)r=getMaximum(node.rChild,max);
+    if(l>max)max = l;
+    if(r>max)max = r;
+    return max;
+  }
+  //get minimum
+  public int getMinimum(BTreeNode node,int min){
+    if(node ==null)return -1;
+    if(node.value<min)min = node.value;
+    int l=100;
+    int r=100;
+    if(node.lChild!=null)l= getMinimum(node.lChild,min);
+    if(node.rChild!=null)r= getMinimum(node.rChild,min);
+    if(l<min)min = l;
+    if(r<min)min = r;
+    return min;
+  }
+  
+  //to Check if the current tree is a BST
+  public boolean isBST(BTreeNode node){
+    if(node ==null)return true;
+    int max=0;
+    int min=100;
+    max = getMaximum(node,0);
+    min = getMinimum(node,100);
+    System.out.println("node "+node.value+" max "+max+" min "+min);
+    if(node.value<=max && node.value>=min && isBST(node.lChild) && isBST(node.rChild))return true;
+    else return false;
+  }
 }
