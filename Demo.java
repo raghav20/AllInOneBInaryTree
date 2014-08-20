237244436a88e687fd623f682c3d145962b66bb1
Prashant <prash.raaghav@gmail.com>
2014-08-20 18:53:31 -0400

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,30 +1,29 @@
+import java.io.*;
 public class Demo{
-	public static void main(String args[]){
-		int a[] = {2,5,4,7,1,9,3};
-		int post[] = {20,8,4,12,10,14,22,25};
-		int sum[] ={6,3,5,2,5,7,4};
-		BinaryTree bt = new BinaryTree();
-		LinkList ll = new LinkList();
+   public static void main(String args[]){
+      try{
+        BTree bt = new BTree();
 
-		for(int i=0;i<post.length;i++){
-			bt.insert(post[i]);
+        int s[] = {10,6,14,5,8,11,18};
+        //int s[]= {20,8,4,10,22,12,14,25};
 
-		}
-		bt.show();
-		ll.add(100);
-		ll.add(200);
-		ll.add(300);
-		ll.add(200);
-		ll.add(100);
-		ll.add(100);
-		//ll.display();
+        for(int a:s){
+          bt.insert(a);
+        }
+        bt.inOrder(bt.root);
+        //bt.getDFS(bt.root);
+        //bt.hasPathSum(bt.root,42);
+        //bt.printLeaves(bt.root);
+        //bt.inOrderI(bt.root);
+        //bt.bfs(bt.root);
+        //bt.boundry(bt.root);
+        //bt.inOrder(bt.root);
+        bt.show(bt.root);
+        //System.out.println("\ntest "+   bt.maxPathLeaf(bt.root,0) +" "+ bt.min);
+      }
+      catch(Exception e){
+        e.printStackTrace();
+      }
 
-		//Fib f = new Fib();
-		//System.out.println(f.getFib3(5));
-		
-		//System.out.println("has seq..." + bt.hasSequence(post,0,7));
-		//System.out.println("has bst..." + bt.isBST());
-
-	}
-
+   }
 }
